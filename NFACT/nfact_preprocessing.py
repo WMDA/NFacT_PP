a063ec408baf59d8e895c33f5496c3864543cbbe
Daniel Halls <mszdjh3@mslaprad03.nottingham.ac.uk>
2024-06-12 15:44:50 +0100

    test

--- NFACT/nfact_preprocessing.py
+++ NFACT/nfact_preprocessing.py
@@ -1,1 +1,49 @@
-import argparse 
+import argparse
+
+
+def args() -> dict:
+    """
+    Function to get arguements
+    to run NFACT pre-processing
+
+    Parameters
+    -----------
+    None
+
+    Returns
+    -------
+    dict: dictionary object
+        dict of arguments
+    """
+    option = argparse.ArgumentParser()
+    option.add_argument(
+        "-s"
+        "--study_folder"
+        "helllllllllllllllldfdfdfdfdfdfdfdflldfdfdfddfdfdfdfdfdfdfdfddfdfll"
+    )
+    return vars(option.parse_args())
+
+
+def splash() -> str:
+    """
+    Function to return NFACT splash
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    str: splash
+    """
+    return """
+
+##    ## ########    ###     ######  ########         ########  ########
+###   ## ##         ## ##   ##    ##    ##            ##     ## ##     ##
+####  ## ##        ##   ##  ##          ##            ##     ## ##     ##
+## ## ## ######   ##     ## ##          ##            ########  ########
+##  #### ##       ######### ##          ##            ##        ##
+##   ### ##       ##     ## ##    ##    ##            ##        ##
+##    ## ##       ##     ##  ######     ##            ##        ##
+
+"""
